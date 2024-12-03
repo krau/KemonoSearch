58a37c9f1cf39af39e53a4e0f45a72dd5d8b3a19
krau <71133316+krau@users.noreply.github.com>
2024-12-03 10:10:15 +0800

    feat: init commit

---
+++ go.mod
@@ -0,0 +1,77 @@
+module KemonoSearch
+
+go 1.23.3
+
+require github.com/ncruces/go-sqlite3/gormlite v0.20.3
+
+require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/PuerkitoBio/purell v1.2.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/andybalholm/brotli v1.1.1 // indirect
+	github.com/bytedance/sonic v1.12.5 // indirect
+	github.com/bytedance/sonic/loader v0.2.1 // indirect
+	github.com/cloudflare/circl v1.5.0 // indirect
+	github.com/cloudwego/base64x v0.1.4 // indirect
+	github.com/cloudwego/iasm v0.2.0 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.7 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.10.0 // indirect
+	github.com/go-openapi/jsonpointer v0.21.0 // indirect
+	github.com/go-openapi/jsonreference v0.21.0 // indirect
+	github.com/go-openapi/spec v0.21.0 // indirect
+	github.com/go-openapi/swag v0.23.0 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.23.0 // indirect
+	github.com/go-task/slim-sprig/v3 v3.0.0 // indirect
+	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/google/pprof v0.0.0-20241128161848-dc51965c6481 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/imroc/req/v3 v3.48.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.17.11 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/ncruces/go-sqlite3 v0.20.3 // indirect
+	github.com/ncruces/julianday v1.0.0 // indirect
+	github.com/onsi/ginkgo/v2 v2.22.0 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/quic-go/qpack v0.5.1 // indirect
+	github.com/quic-go/quic-go v0.48.2 // indirect
+	github.com/refraction-networking/utls v1.6.7 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
+	github.com/swaggo/files v1.0.1 // indirect
+	github.com/swaggo/gin-swagger v1.6.0 // indirect
+	github.com/swaggo/swag v1.16.4 // indirect
+	github.com/tetratelabs/wazero v1.8.2 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.12 // indirect
+	github.com/urfave/cli/v2 v2.27.5 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
+	go.uber.org/mock v0.5.0 // indirect
+	golang.org/x/arch v0.12.0 // indirect
+	golang.org/x/crypto v0.29.0 // indirect
+	golang.org/x/exp v0.0.0-20241108190413-2d47ceb2692f // indirect
+	golang.org/x/mod v0.22.0 // indirect
+	golang.org/x/net v0.31.0 // indirect
+	golang.org/x/sync v0.9.0 // indirect
+	golang.org/x/sys v0.27.0 // indirect
+	golang.org/x/text v0.20.0 // indirect
+	golang.org/x/tools v0.27.0 // indirect
+	google.golang.org/protobuf v1.35.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	gorm.io/gorm v1.25.12 // indirect
+	sigs.k8s.io/yaml v1.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	_ "KemonoSearch/docs"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/bytedance/sonic"
+	"github.com/gin-gonic/gin"
+	"github.com/imroc/req/v3"
+	_ "github.com/ncruces/go-sqlite3/embed"
+	"github.com/ncruces/go-sqlite3/gormlite"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"gorm.io/gorm"
+)
+
+type Creator struct {
+	Link      string `json:"link"`
+	CreatorID string `json:"id" gorm:"primaryKey"`
+	Name      string `json:"name" gorm:"index"`
+	Service   string `json:"service" gorm:"primaryKey"`
+	Indexed   int    `json:"indexed"`
+	Updated   int    `json:"updated"`
+	Favorited int    `json:"favorited"`
+}
+
+var CreatorsFileUrl = "https://kemono.su/api/v1/creators.txt"
+var db *gorm.DB
+
+func main() {
+	var err error
+	db, err = gorm.Open(gormlite.Open(GetDefaultEnv("KEMONOSEARCH_DB", "creators.db")))
+	if err != nil {
+		panic(err)
+	}
+	if err := db.AutoMigrate(&Creator{}); err != nil {
+		panic(err)
+	}
+
+	go func() {
+		for {
+			fmt.Println("syncing creators...")
+			if err := SyncCreators(); err != nil {
+				fmt.Println(err)
+			}
+			time.Sleep(time.Hour * 24)
+		}
+	}()
+
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	router.GET("/creator", SearchCreators)
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	router.Run(GetDefaultEnv("KEMONOSEARCH_ADDR", ":39808"))
+}
+
+// @Summary Search creators
+// @Description Search creators by name
+// @Accept json
+// @Produce json
+// @Param name query string true "Creator name"
+// @Success 200 {array} Creator
+// @Router /creator [get]
+func SearchCreators(ctx *gin.Context) {
+	name := ctx.Query("name")
+	if name == "" {
+		ctx.JSON(400, gin.H{"error": "name is required"})
+		return
+	}
+	var creators []Creator
+	if err := db.Where("name LIKE ?", "%"+name+"%").Find(&creators).Error; err != nil {
+		ctx.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+	ctx.JSON(200, creators)
+}
+
+func SyncCreators() error {
+	resp, err := req.R().Get(CreatorsFileUrl)
+	if err != nil {
+		return err
+	}
+	if resp.IsErrorState() {
+		return fmt.Errorf("http status: %s", resp.GetStatus())
+	}
+	defer resp.Body.Close()
+	decoder := sonic.ConfigDefault.NewDecoder(resp.Body)
+	creators := make([]Creator, 0)
+	err = decoder.Decode(&creators)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("decoded creators: %d\n", len(creators))
+
+	var count int64
+	if err := db.Model(&Creator{}).Count(&count).Error; err != nil {
+		return err
+	}
+	fmt.Printf("total creators in db: %d\n", count)
+
+	tx := db.Begin()
+	for _, creator := range creators {
+		switch creator.Service {
+		case "patreon":
+			creator.Link = fmt.Sprintf("https://kemono.su/patreon/user/%s", creator.CreatorID)
+		case "fanbox":
+			creator.Link = fmt.Sprintf("https://kemono.su/fanbox/user/%s", creator.CreatorID)
+		case "discord":
+			creator.Link = fmt.Sprintf("https://kemono.su/discord/server/%s", creator.CreatorID)
+		case "fantia":
+			creator.Link = fmt.Sprintf("https://kemono.su/fantia/user/%s", creator.CreatorID)
+		case "boosty":
+			creator.Link = fmt.Sprintf("https://kemono.su/boosty/user/%s", creator.CreatorID)
+		case "subscribestar":
+			creator.Link = fmt.Sprintf("https://kemono.su/subscribestar/user/%s", creator.CreatorID)
+		case "dlsite":
+			creator.Link = fmt.Sprintf("https://kemono.su/dlsite/user/%s", creator.CreatorID)
+		}
+		if err := tx.Save(&creator).Error; err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		return err
+	}
+
+	if err := db.Model(&Creator{}).Count(&count).Error; err != nil {
+		return err
+	}
+	fmt.Printf("update success, total creators in db: %d\n", count)
+	return nil
+}
+
+func GetDefaultEnv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if value == "" {
+		return defaultValue
+	}
+	return value
+}
